502d628dd41c346a0c5d792b0f558a8d72a5b28c
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-09-06 00:23:14 +0300

    doneupload

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@ import ProgressBar from "@/components/progressBar/ProgressBar";
 import Slider from "@/components/slider/Slider";
 import VideoLibTabs from "@/components/videoLibTabs/VideoLibTabs";
 import VideoTabs from "@/components/videoLibTabs/VideoTabs";
+export const dynamic = 'force-static';
 
 
 export default function Home() {
   return (
-    <main className="w-full">
-      {/* <Slider /> */}
-      {/* <VideoTabs/> */}
-      <ProgressBar/>
-    </main>
+    <div>
+      <main className="w-full">
+        {/* <Slider />
+        <VideoTabs/> */}
+        <ProgressBar />
+      </main>
+    </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-      domains: [''],
-    },
-  }
+  output: 'export',
+  images: {
+    unoptimized: true,
+    domains: [''],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
