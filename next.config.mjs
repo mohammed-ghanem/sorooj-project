a1b2ec9a94b21694d07059acec22a0517e868da8
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-09-11 00:50:33 +0300

    ddd

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   output: 'export',
+  // experimental: {
+  //   appDir: true, // Ensure the App Router feature is enabled
+  // },
+  // trailingSlash: true, // Important for static hosting
   images: {
     unoptimized: true,
     domains: [''],
   },
+
 }
 
 export default nextConfig
