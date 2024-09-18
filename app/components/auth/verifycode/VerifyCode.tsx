4715bf3c59149de52405f7607c4e978d91ffea15
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-09-18 09:52:17 +0200

    v

--- app/components/auth/verifycode/VerifyCode.tsx
+++ app/components/auth/verifycode/VerifyCode.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from "react"
+import { useState, useEffect, Suspense } from "react"
 import { useSearchParams, useRouter } from "next/navigation"
 import axios from "axios"
 import Swal from "sweetalert2"
@@ -93,33 +93,36 @@ const VerifyCode = () => {
   }
 
   return (
-    <div>
-      <h2>Verify Your Code</h2>
-      <form onSubmit={handleVerifyCode} className="space-y-4">
-        <div>
-          <label htmlFor="verification-code" className="block text-sm font-medium">
-            Enter the verification code sent to {email}
-          </label>
-          <input
-            type="text"
-            id="verification-code"
-            value={verificationCode}
-            onChange={(e) => setVerificationCode(e.target.value)}
-            className="w-full p-2 border rounded"
-            required
-          />
-        </div>
+    <Suspense fallback={<div>loading ...</div>}>
+      <div>
+        <h2>Verify Your Code</h2>
+        <form onSubmit={handleVerifyCode} className="space-y-4">
+          <div>
+            <label htmlFor="verification-code" className="block text-sm font-medium">
+              Enter the verification code sent to {email}
+            </label>
+            <input
+              type="text"
+              id="verification-code"
+              value={verificationCode}
+              onChange={(e) => setVerificationCode(e.target.value)}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
 
-        {error && <p className="text-red-500">{error}</p>}
+          {error && <p className="text-red-500">{error}</p>}
 
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-        >
-          Verify Code
-        </button>
-      </form>
-    </div>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          >
+            Verify Code
+          </button>
+        </form>
+      </div>
+    </Suspense>
+
   );
 }
 
