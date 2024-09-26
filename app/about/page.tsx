7a5b41e58808bd217b08c5c0a19f273cda444cf1
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-09-26 12:20:48 +0200

    still lang

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,10 @@
 import { useTranslations } from 'next-intl';
 
 const Page = () => {
-  const t = useTranslations('About');
-  
+
   return (
     <div>
-      <h1>{t("title") }</h1>
+      about page
     </div>
   )
 }
