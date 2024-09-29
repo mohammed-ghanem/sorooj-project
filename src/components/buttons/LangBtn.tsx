bf560a988f886196aeb0786c98fe1b1c3a3c6d43
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-09-29 13:49:42 +0200

    still

--- src/components/buttons/LangBtn.tsx
+++ src/components/buttons/LangBtn.tsx
@@ -1,11 +1,10 @@
 
-
 import React from 'react'
 
 const LangBtn = () => {
   return (
     <div>
-      <select onChange={(e)=> console.log(e.target.value)}>
+      <select >
         <option value="en">English</option>
         <option value="ar">arabic</option>
       </select>
@@ -14,5 +13,33 @@ const LangBtn = () => {
 }
 
 export default LangBtn
+
+// 'use client';
+
+// import React from 'react';
+// import Cookies from 'js-cookie';
+
+// const LangBtn = () => {
+//   const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+//     const newLang = e.target.value;
+//     Cookies.set('language', newLang, { path: '/' }); // Set cookie
+//     console.log(`Cookie set: language=${newLang}`); // Debugging line
+
+//     // Force a reload to ensure the server recognizes the cookie change
+//     window.location.reload();
+//   };
+
+//   return (
+//     <div>
+//       <select onChange={handleLanguageChange}>
+//         <option value="en">English</option>
+//         <option value="ar">Arabic</option>
+//         <option value="en">English</option>
+//       </select>
+//     </div>
+//   );
+// };
+
+// export default LangBtn;
 
 
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,36 +1,3 @@
-// import { getRequestConfig } from 'next-intl/server';
-// import { cookies } from 'next/headers';
-
-// export default getRequestConfig(async () => {
-//   const languageCookie = cookies().get('language')?.value; // Get the language from cookie
-
-//   console.log(`Current language from cookie: ${languageCookie}`); // Debug
-
-//   const locale = languageCookie || 'en'; // Default to 'en' if cookie is undefined
-
-//   return {
-//     locale,
-//     messages: (await import(`../../messages/${locale}.json`)).default
-//   };
-// });
-
-
-// import { getRequestConfig } from 'next-intl/server';
-// import { cookies } from 'next/headers';
- 
-// export default getRequestConfig(async () => {
-//   // Provide a static locale, fetch a user setting,
-//   // read from `cookies()`, `headers()`, etc.
-//     const test = cookies().get('language')
-//     console.log(`test ${test}`)
-//   const locale = 'en';
- 
-//   return {
-//     locale,
-//     messages: (await import(`../../messages/${locale}.json`)).default
-//   };
-// });
-
 
 
 // import { getRequestConfig } from 'next-intl/server';
