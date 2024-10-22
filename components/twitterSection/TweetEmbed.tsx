cc7d46b7b0915dd3966a4271dfadfdb2da1a12a1
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-10-22 19:49:49 +0300

    sectiontwdone

---
+++ components/twitterSection/TweetEmbed.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+
+const TweetEmbed = () => {
+    return (
+        <div>
+            <TwitterTweetEmbed tweetId={'1767537775748735200'} />
+        </div>
+    );
+};
+
+export default TweetEmbed;
--- components/twitterSection/TwitterSection.tsx
+++ components/twitterSection/TwitterSection.tsx
@@ -1,35 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
 import Answers from './Answers'
+import TweetEmbed from './TweetEmbed'
 
 const TwitterSection = () => {
     return (
-        <div className='container mx-auto grid grid-rows-3 grid-flow-col gap-4'>
-            <div className='answ&Ques row-span-2'>
+        <div className='container mx-auto w-[80%] grid grid-cols-3 gap-10'>
+            <div className='answ&Ques col-span-2'>
                 <Answers />
             </div>
-
-            <div className='twitterPosts row-span-3'>
-                twitter posts
-                {/* <iframe
-                    loading="lazy"
-                    id="twitter-widget-0"
-                    scrolling="no"
-                    frameBorder="0"
-                    allowFullScreen
-                    className="block w-[360px] h-[500px] flex-grow overflow-scroll"
-                    title="Twitter Timeline"
-                    src="https://syndication.twitter.com/srv/timeline-profile/screen-name/soroojcenter?dnt=false&amp;embedId=twitter-widget-0&amp;features=eyJ0ZndfdGltZWxpbmVfbGlzdCI6eyJidWNrZXQiOlsibGlua3RyLmVlIiwidHIuZWUiLCJ0ZXJyYS5jb20uYnIiLCJ3d3cubGlua3RyLmVlIiwid3d3LnRyLmVlIiwid3d3LnRlcnJhLmNvbS5iciJdLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X2hvcml6b25fdGltZWxpbmVfMTIwMzQiOnsiYnVja2V0IjoidHJlYXRtZW50IiwidmVyc2lvbiI6bnVsbH0sInRmd190d2VldF9lZGl0X2JhY2tlbmQiOnsiYnVja2V0Ijoib24iLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X3JlZnNyY19zZXNzaW9uIjp7ImJ1Y2tldCI6Im9uIiwidmVyc2lvbiI6bnVsbH0sInRmd19zaG93X2J1c2luZXNzX3ZlcmlmaWVkX2JhZGdlIjp7ImJ1Y2tldCI6Im9uIiwidmVyc2lvbiI6bnVsbH0sInRmd19jaGluX3BpbGxzXzE0NzQxIjp7ImJ1Y2tldCI6ImNvbG9yX2ljb25zIiwidmVyc2lvbiI6bnVsbH0sInRmd190d2VldF9yZXN1bHRfbWlncmF0aW9uXzEzOTc5Ijp7ImJ1Y2tldCI6InR3ZWV0X3Jlc3VsdCIsInZlcnNpb24iOm51bGx9LCJ0Zndfc2Vuc2l0aXZlX21lZGlhX2ludGVyc3RpdGlhbF8xMzk2MyI6eyJidWNrZXQiOiJpbnRlcnN0aXRpYWwiLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X2V4cGVyaW1lbnRzX2Nvb2tpZV9leHBpcmF0aW9uIjp7ImJ1Y2tldCI6MTIwOTYwMCwidmVyc2lvbiI6bnVsbH0sInRmd19kdXBsaWNhdGVfc2NyaWJlc190b19zZXR0aW5ncyI6eyJidWNrZXQiOiJvbiIsInZlcnNpb24iOm51bGx9LCJ0ZndfdmlkZW9faGxzX2R5bmFtaWNfbWFuaWZlc3RzXzE1MDgyIjp7ImJ1Y2tldCI6InRydWVfYml0cmF0ZSIsInZlcnNpb24iOm51bGx9LCJ0Zndfc2hvd19ibHVlX3ZlcmlmaWVkX2JhZGdlIjp7ImJ1Y2tldCI6Im9uIiwidmVyc2lvbiI6bnVsbH0sInRmd19zaG93X2dvdl92ZXJpZmllZF9iYWRnZSI6eyJidWNrZXQiOiJvZmYiLCJ2ZXJzaW9uIjpudWxsfSwidGZ3X3Nob3dfYnVzaW5lc3NfYWZmaWxpYXRlX2JhZGdlIjp7ImJ1Y2tldCI6Im9mZiIsInZlcnNpb24iOm51bGx9LCJ0ZndfdHdlZXRfZWRpdF9mcm9udGVuZCI6eyJidWNrZXQiOiJvbiIsInZlcnNpb24iOm51bGx9fQ%3D%3D&amp;frame=false&amp;hideBorder=false&amp;hideFooter=false&amp;hideHeader=false&amp;hideScrollBar=false&amp;lang=ar&amp;origin=https%3A%2F%2Fhamadalhajri.net%2F&amp;sessionId=6dbe317b63cb46451a94d3afb6172c4ff09356b1&amp;showHeader=true&amp;showReplies=false&amp;transparent=false&amp;widgetsVersion=a3525f077c700%3A1667415560940"
-                    />
-
-                    <div
-                    id="ascrail2000-hr"
-                    className="nicescroll-rails fixed left-0 bottom-0 w-[60%] h-2.5 z-[2999] opacity-100 hidden cursor-default"
-                    >
-                    <div
-                        className="relative top-0 w-[1536px] h-2.5 bg-[#f09f05] rounded-none"
-                    ></div>
-                    </div> */}
+            <div className='twitterPosts'>
+                <TweetEmbed />
             </div>
         </div>
     )
