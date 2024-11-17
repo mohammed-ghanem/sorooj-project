1331e9f129ca4e310507409b1bda9b13009cd3c6
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-11-17 20:52:50 +0200

    timeforbtn

--- components/header/LogoNav.tsx
+++ components/header/LogoNav.tsx
@@ -1,55 +1,60 @@
-'use client';
+import BtnLogin from "../Btnlogin/BtnLogin"
+import Logo from "../logo/Logo"
+import SerachInput from "../searchBar/SerachInput"
 
-import { useEffect, useState } from 'react';
-import BtnLogin from "../Btnlogin/BtnLogin";
-import Logo from "../logo/Logo";
-import SerachInput from "../searchBar/SerachInput";
+
+
 
 const LogoNav = () => {
-    const [showBtnLogin, setShowBtnLogin] = useState(false);
-
-    useEffect(() => {
-        // Delay the rendering of BtnLogin until after the page has loaded
-        const timer = setTimeout(() => {
-            setShowBtnLogin(true);
-        }, 2000); // You can adjust the delay (e.g., 100ms or more) if needed
-
-        return () => clearTimeout(timer); // Cleanup timer
-    }, []);
-
     return (
         <div className="container bg-white mx-auto row items-center grid grid-cols-1 md:grid-cols-3">
             <Logo />
             <SerachInput />
-            {showBtnLogin && <BtnLogin />}
+            <BtnLogin />
         </div>
+    )
+}
 
-
-    );
-};
-
-export default LogoNav;
-
+export default LogoNav
 
 
 
 
 
-// import BtnLogin from "../Btnlogin/BtnLogin"
-// import Logo from "../logo/Logo"
-// import SerachInput from "../searchBar/SerachInput"
+// 'use client';
 
-
-
+// import { useEffect, useState } from 'react';
+// import BtnLogin from "../Btnlogin/BtnLogin";
+// import Logo from "../logo/Logo";
+// import SerachInput from "../searchBar/SerachInput";
 
 // const LogoNav = () => {
+//     const [showBtnLogin, setShowBtnLogin] = useState(false);
+
+//     useEffect(() => {
+//         // Delay the rendering of BtnLogin until after the page has loaded
+//         const timer = setTimeout(() => {
+//             setShowBtnLogin(true);
+//         }, 2000); // You can adjust the delay (e.g., 100ms or more) if needed
+
+//         return () => clearTimeout(timer); // Cleanup timer
+//     }, []);
+
 //     return (
 //         <div className="container bg-white mx-auto row items-center grid grid-cols-1 md:grid-cols-3">
 //             <Logo />
 //             <SerachInput />
-//             <BtnLogin />
+//             {showBtnLogin && <BtnLogin />}
 //         </div>
-//     )
-// }
 
-// export default LogoNav
+
+//     );
+// };
+
+// export default LogoNav;
+
+
+
+
+
+
