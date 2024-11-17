f4ca80286a3989f43e3bbe8442b9e8622e4ffbcb
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-11-17 13:38:38 +0200

    handelcourses

--- app/[lang]/courses/page.tsx
+++ app/[lang]/courses/page.tsx
@@ -18,63 +18,65 @@ const page = () => {
     <div>
       <div>
         <div>
-          <Banners src={defImage} textPath="الدورات المجانية"/>
+          <Banners src={defImage} textPath="الدورات المجانية" />
         </div>
-        <div className="coursesContainer container mx-auto my-20 grid grid-cols-4 gap-10 w-[80%]">
-          <div className="">
+        <div className="coursesContainer container mx-auto my-20 grid grid-cols-1 lg:grid-cols-4 gap-0 lg:gap-10 w-[95%] lg:w-[80%]">
+          <div className="my-5 lg:my-0">
             <CategoriesBox />
           </div>
-          <div className="col-span-3 grid grid-cols-3 gap-4">
-            <CoursesCard
-              imgSrc={test}
-              watchNumber={"120 مشاهدة"}
-              datePublish={"24 اغسطس 2024"}
-              courseTitle={"شرح الفقة الوسطى"}
-              doctorName={"الدكتور حمد بن محمد الهاجرى"}
-              descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
-              likeBtn={"like"}
-              pathLinkToContent={`/${lang}/courses/${coursesId}`}
-            />
-            <CoursesCard
-              imgSrc={test}
-              watchNumber={"120 مشاهدة"}
-              datePublish={"24 اغسطس 2024"}
-              courseTitle={"شرح الفقة الوسطى"}
-              doctorName={"الدكتور حمد بن محمد الهاجرى"}
-              descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
-              likeBtn={"like"}
-              pathLinkToContent={`/${lang}/courses/${coursesId}`}
-            />
-            <CoursesCard
-              imgSrc={test}
-              watchNumber={"120 مشاهدة"}
-              datePublish={"24 اغسطس 2024"}
-              courseTitle={"شرح الفقة الوسطى"}
-              doctorName={"الدكتور حمد بن محمد الهاجرى"}
-              descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
-              likeBtn={"like"}
-              pathLinkToContent={`/${lang}/courses/${coursesId}`}
-            />
-            <CoursesCard
-              imgSrc={test}
-              watchNumber={"120 مشاهدة"}
-              datePublish={"24 اغسطس 2024"}
-              courseTitle={"شرح الفقة الوسطى"}
-              doctorName={"الدكتور حمد بن محمد الهاجرى"}
-              descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
-              likeBtn={"like"}
-              pathLinkToContent={`/${lang}/courses/${coursesId}`}
-            />
-            <CoursesCard
-              imgSrc={test}
-              watchNumber={"120 مشاهدة"}
-              datePublish={"24 اغسطس 2024"}
-              courseTitle={"شرح الفقة الوسطى"}
-              doctorName={"الدكتور حمد بن محمد الهاجرى"}
-              descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
-              likeBtn={"like"}
-              pathLinkToContent={`/${lang}/courses/${coursesId}`}
-            />
+          <div className=" lg:col-span-3">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              <CoursesCard
+                imgSrc={test}
+                watchNumber={"120 مشاهدة"}
+                datePublish={"24 اغسطس 2024"}
+                courseTitle={"شرح الفقة الوسطى"}
+                doctorName={"الدكتور حمد بن محمد الهاجرى"}
+                descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
+                likeBtn={"like"}
+                pathLinkToContent={`/${lang}/courses/${coursesId}`}
+              />
+              <CoursesCard
+                imgSrc={test}
+                watchNumber={"120 مشاهدة"}
+                datePublish={"24 اغسطس 2024"}
+                courseTitle={"شرح الفقة الوسطى"}
+                doctorName={"الدكتور حمد بن محمد الهاجرى"}
+                descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
+                likeBtn={"like"}
+                pathLinkToContent={`/${lang}/courses/${coursesId}`}
+              />
+              <CoursesCard
+                imgSrc={test}
+                watchNumber={"120 مشاهدة"}
+                datePublish={"24 اغسطس 2024"}
+                courseTitle={"شرح الفقة الوسطى"}
+                doctorName={"الدكتور حمد بن محمد الهاجرى"}
+                descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
+                likeBtn={"like"}
+                pathLinkToContent={`/${lang}/courses/${coursesId}`}
+              />
+              <CoursesCard
+                imgSrc={test}
+                watchNumber={"120 مشاهدة"}
+                datePublish={"24 اغسطس 2024"}
+                courseTitle={"شرح الفقة الوسطى"}
+                doctorName={"الدكتور حمد بن محمد الهاجرى"}
+                descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
+                likeBtn={"like"}
+                pathLinkToContent={`/${lang}/courses/${coursesId}`}
+              />
+              <CoursesCard
+                imgSrc={test}
+                watchNumber={"120 مشاهدة"}
+                datePublish={"24 اغسطس 2024"}
+                courseTitle={"شرح الفقة الوسطى"}
+                doctorName={"الدكتور حمد بن محمد الهاجرى"}
+                descriptionCourse={"برنامج محطات فى العقيدة المحور الثانى"}
+                likeBtn={"like"}
+                pathLinkToContent={`/${lang}/courses/${coursesId}`}
+              />
+            </div>
           </div>
         </div>
       </div>
--- components/banners/Banners.tsx
+++ components/banners/Banners.tsx
@@ -15,7 +15,7 @@ const Banners: React.FC<BannersProps> = ({ src, textPath }) => {
         <div>
             <div className="w-full h-[150px] lg:h-[220px] relative overflow-hidden">
                 <Image src={src} fill alt="banner" />
-                <span className="absolute bkPrimaryColor  px-[2px] md:px-[20px] py-[10px] rounded-tl-[15px] rounded-br-none rounded-tr-[15px] rounded-bl-none left-2/4 -bottom-[22px] -translate-x-1/2 -translate-y-1/2 text-center text-white">
+                <span className="absolute bkPrimaryColor w-56  px-[2px] md:px-[20px] py-[10px] rounded-tl-[15px] rounded-br-none rounded-tr-[15px] rounded-bl-none left-2/4 -bottom-[22px] -translate-x-1/2 -translate-y-1/2 text-center text-white">
                     <span>{translate ? translate.navigation.home : ""}</span>
                     <span> -- </span>
                     <span>{textPath}</span>
