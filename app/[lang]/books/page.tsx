d1e492b48bdfde8c1623df2822795216ef828044
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-12-17 06:14:19 +0200

    newBookSlide

--- app/[lang]/books/page.tsx
+++ app/[lang]/books/page.tsx
@@ -2,20 +2,20 @@ import BookCardPage from '@/components/bookCardPage/BookCardPage'
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "دورات تعليمية بمركز سرج للدراسات والابحاث",
-  description: "دورات تعليمية مجانية منارة البحث العلمى والمذاهب الفكرية المعاصرة",
-  keywords: ",دورات,تعليمية,مركز,سرج,للدراسات,والابحاث,المذاهب,الفكرية,المعاصرة",
+  title: " كتب وابحاث مركز سرج للدراسات والابحاث",
+  description: "  كتب وابحاث مركز سرج منارة البحث العلمى والمذاهب الفكرية المعاصرة",
+  keywords: ",كتب,ابحاث,مركز,سرج,للدراسات,والابحاث,المذاهب,الفكرية,المعاصرة",
   openGraph: {
     title: "دورات تعليمية بمركز سرج للدراسات والابحاث",
-    description: "دورات تعليمية مجانية منارة البحث العلمى والمذاهب الفكرية المعاصرة",
+    description: "كتب وابحاث  مركز سرج منارة البحث العلمى والمذاهب الفكرية المعاصرة",
     images: [
-          {
-              url: "https://www.sorooj.org/_next/static/media/logo.2da5fe49.png",
-              alt: " مركز سرج للدراسات والابحاث",
-              width: 1200,
-              height: 630,
-          },
-      ],
+      {
+        url: "https://www.sorooj.org/_next/static/media/logo.2da5fe49.png",
+        alt: " مركز سرج للدراسات والابحاث",
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
 };
 const page = () => {
