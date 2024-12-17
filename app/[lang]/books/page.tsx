9bd62de9fa91fdee7af5aab4bc99fc287f04e730
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-12-17 06:10:23 +0200

    newBookSlide

--- app/[lang]/books/page.tsx
+++ app/[lang]/books/page.tsx
@@ -1,24 +1,6 @@
-'use client'
-import BookCardPage from '@/components/bookCardPage/BookCardPage'
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "كتب وابحاث مركز سرج للدرسات والابحاث ",
-  description: " كتب وابحاث مركز سرج كافة الاخبار والاحداث والدروس والمحاضرات المتعلفة بمركز سرج للدراسات والبحث العلمى منارة وفهم المذاهب الفكرية المعاصرة",
-  keywords: "مركز,سرج,للدراسات,والابحاث,المذاهب,الفكرية,كتب,ابحاث,المعاصرة",
-  openGraph: {
-    title: "كتب وابحاث مركز سرج للدراسات والابحاث",
-    description: "كتب وابحاث مركز سرج للدراسات والابحاث منارة البحث العلمى والمذاهب الفكرية المعاصرة",
-    images: [
-      {
-        url: "https://www.sorooj.org/_next/static/media/logo.2da5fe49.png",
-        alt: "كتب وابحاث مركز سرج للدراسات والابحاث",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-};
+import BookCardPage from '@/components/bookCardPage/BookCardPage'
+
 
 const page = () => {
   return (
