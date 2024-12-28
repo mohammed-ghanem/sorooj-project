42aaf06b8a6043832b716713e4fd7f2a9173c1db
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2024-12-28 18:43:19 +0200

    googlelogin

--- components/socialLogin/SocialLogin.tsx
+++ components/socialLogin/SocialLogin.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-import axios from "axios";
+import { parse } from "querystring";
 import Cookies from "js-cookie";
 import facebook from "@/public/assets/images/facebook.svg";
 import google from "@/public/assets/images/google.svg";
@@ -8,41 +8,28 @@ import Image from "next/image";
 import TranslateHook from "../translate/TranslateHook";
 
 const SocialLogin = () => {
-     const translate = TranslateHook();
-    // const GOOGLE_LOGIN_URL = `https://dashboard.sorooj.org/client-api/v1/auth/google/redirect`;
+    const translate = TranslateHook();
 
-    // useEffect(() => {
-    //     const handleToken = async () => {
-    //         try {
-    //             // Extract the token from the URL
-    //             const urlParams = new URLSearchParams(window.location.search);
-    //             const token = urlParams.get("token");
+    useEffect(() => {
+        // Check if the URL contains a token in the query string
+        const queryString = window.location.search;
+        if (queryString) {
+            const params = new URLSearchParams(queryString);
+            const token = params.get("token");
+            if (token) {
+                // Store the token in a cookie
+                Cookies.set("access_token", token, { expires: 7, secure: true });
+                // Redirect to the home page
+                window.location.href = "https://www.sorooj.org";
+            }
+        }
+    }, []);
 
-    //             if (token) {
-    //                 // Debugging: log the token
-    //                 console.log("Token:", token);
-
-    //                 // Store the token in cookies
-    //                 Cookies.set("access_token", token, {
-    //                     expires: 7,
-    //                     domain: "sorooj.org",
-    //                 });
-
-    //                 // Debugging: verify the cookie
-    //                 console.log("Cookie set:", Cookies.get("access_token"));
-
-    //                 // Redirect to the home page
-    //                 setTimeout(() => {
-    //                     window.location.href = "/";
-    //                 }, 500);
-    //             }
-    //         } catch (error) {
-    //             console.error("An error occurred while handling the token:", error);
-    //         }
-    //     };
-
-    //     handleToken();
-    // }, []);
+    const handleLogin = (provider: string) => {
+        const baseUrl = "https://dashboard.sorooj.org/client-api/v1/auth";
+        const redirectUrl = `${baseUrl}/${provider}/redirect`;
+        window.location.href = redirectUrl;
+    };
 
     return (
         <div className="text-center">
@@ -52,17 +39,23 @@ const SocialLogin = () => {
                 ــــــــــــــــــــــــــــــــــــــــــ
             </p>
             <div className="socialLogin w-[95%] md:w-[70%] mx-auto my-6 grid grid-cols-1 md:grid-cols-2 gap-1">
-                <div className="bg-[#faf9f6] p-[10px] rounded-[5px] [box-shadow:1px_1px_6px_#ddd]">
-                    <a href="#" className="flex items-center justify-center text-sm">
+                <div
+                    className="bg-[#faf9f6] p-[10px] rounded-[5px] [box-shadow:1px_1px_6px_#ddd] cursor-pointer"
+                    onClick={() => handleLogin("facebook")}
+                >
+                    <div className="flex items-center justify-center text-sm">
                         {translate ? translate.socialLogin.facebook : ""}
                         <Image src={facebook} className="ml-2" width={30} height={30} alt="facebook icon" />
-                    </a>
+                    </div>
                 </div>
-                <div className="bg-[#faf9f6] p-[10px] rounded-[5px] [box-shadow:1px_1px_6px_#ddd]">
-                    <a href={''} className="flex items-center justify-center">
+                <div
+                    className="bg-[#faf9f6] p-[10px] rounded-[5px] [box-shadow:1px_1px_6px_#ddd] cursor-pointer"
+                    onClick={() => handleLogin("google")}
+                >
+                    <div className="flex items-center justify-center">
                         {translate ? translate.socialLogin.google : ""}
                         <Image src={google} className="ml-2" width={30} height={30} alt="google icon" />
-                    </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -70,6 +63,47 @@ const SocialLogin = () => {
 };
 
 export default SocialLogin;
+
+
+
+
+
+
+
+// "use client";
+// import facebook from "@/public/assets/images/facebook.svg";
+// import google from "@/public/assets/images/google.svg";
+// import Image from "next/image";
+// import TranslateHook from "../translate/TranslateHook";
+
+// const SocialLogin = () => {
+//      const translate = TranslateHook();
+//     return (
+//         <div className="text-center">
+//             <p className="text-gray-300">
+//                 ــــــــــــــــــــــــــــــــــــــــــ
+//                 <span className="mainColor mx-2">{translate ? translate.socialLogin.or : ""}</span>
+//                 ــــــــــــــــــــــــــــــــــــــــــ
+//             </p>
+//             <div className="socialLogin w-[95%] md:w-[70%] mx-auto my-6 grid grid-cols-1 md:grid-cols-2 gap-1">
+//                 <div className="bg-[#faf9f6] p-[10px] rounded-[5px] [box-shadow:1px_1px_6px_#ddd]">
+//                     <a href="#" className="flex items-center justify-center text-sm">
+//                         {translate ? translate.socialLogin.facebook : ""}
+//                         <Image src={facebook} className="ml-2" width={30} height={30} alt="facebook icon" />
+//                     </a>
+//                 </div>
+//                 <div className="bg-[#faf9f6] p-[10px] rounded-[5px] [box-shadow:1px_1px_6px_#ddd]">
+//                     <a href={''} className="flex items-center justify-center">
+//                         {translate ? translate.socialLogin.google : ""}
+//                         <Image src={google} className="ml-2" width={30} height={30} alt="google icon" />
+//                     </a>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default SocialLogin;
 
 
 
