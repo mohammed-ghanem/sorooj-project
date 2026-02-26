b3a90f6879c49ad4d8bb0a04c5494f7d7fa617bf
Heba-Hassan-Mostafa <hebahassan3093@gmail.com>
2026-02-27 00:09:51 +0200

    searchOutline

--- components/searchBar/SearchInput.tsx
+++ components/searchBar/SearchInput.tsx
@@ -1,15 +1,15 @@
 "use client";
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SearchInput = () => {
-    const [searchQuery, setSearchQuery] = useState('');
+    const [searchQuery, setSearchQuery] = useState("");
     const router = useRouter();
 
     const handleSearch = () => {
-        if (searchQuery.trim() !== '') {
+        if (searchQuery.trim() !== "") {
             // Navigate to the search page with the query as a parameter
             router.push(`/search?query=${encodeURIComponent(searchQuery)}`);
             // Clear the input field
@@ -19,15 +19,15 @@ const SearchInput = () => {
 
     return (
         <div className="w-[95%] mx-auto">
-            <div className="md:w-full relative flex items-center border-2 rounded-md border-regal-blue p-2 text-sm">
+            <div className="md:w-full relative flex items-center border-2 rounded-md border-regal-blue p-3 text-sm">
                 <input
-                    className="block w-full text-gray-900 bg-transparent dark:placeholder-gray-400 dark:text-white
-                    :focus-visible:outline-none outline-0 p-1"
+                    className="block w-full text-gray-900 bg-transparent dark:placeholder-gray-400
+                     dark:text-white outline-none focus:outline-none focus-visible:outline-none focus:ring-0 focus-visible:ring-0"
                     type="text"
                     placeholder="البحث"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
-                    onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
+                    onKeyDown={(e) => e.key === "Enter" && handleSearch()}
                 />
                 <FontAwesomeIcon
                     className="absolute left-0 w-10 h-4 p-4 bg-regal-blue text-white cursor-pointer"
